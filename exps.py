8de3543b2ee91defadad38a7bb8d9237378c50cb
Ziang Zhou <1016064797@qq.com>
2019-03-31 22:08:30 +0800

    Add files via upload

--- exps.py
+++ exps.py
@@ -1,7 +1,4 @@
 
-import os
-path=os.path.abspath(os.path.dirname(__file__))
-os.chdir(path)
 
 import GCNs
 import exps
@@ -9,28 +6,27 @@ import results
 import log
 from data_process import *
 
-import os
-path=os.path.abspath(os.path.dirname(__file__))
-os.chdir(path)
-
-model_input={}
-model_input['settings']={'dataset':'pubmed','training_epoch':200,'early_stopping':100,
-                       'hidden1':16,'learning_rate':0.01 ,'weight_decay':5e-1 ,
-                       'dropout':0.5,
-                       'all_test':False}
-model_input.update(get_dataset(model_input['settings']['dataset']))
-#GCNs.sb
-#print(dir(GCNs))
-exp1_model=GCNs.GCN_kipf(model_input)
-description='A little test for new framework on GCN'
-recorder=statistic_recorder()
-
-for i in range(1):
-    result=exp1_model.run()
-    recorder.insert(result)
 
 
-write_result(description,exp1_model.settings,recorder.statistic)
+def standard_split_GCN_exp(exptime=1,dataset='cora',description='No special description'):
+    model_input={}
+    model_input['settings']={'dataset':dataset,'training_epoch':200,'early_stopping':10,
+                           'hidden1':16,'learning_rate':0.01 ,'weight_decay':5e-1 ,
+                           'dropout':0.5,
+                           'all_test':False}
+    model_input.update(get_dataset(model_input['settings']['dataset']))
+    #GCNs.sb
+    #print(dir(GCNs))
+    exp1_model=GCNs.GCN_kipf(model_input)
+    description='A little test for new framework on GCN'
+    recorder=statistic_recorder()
+    
+    for i in range(exptime):
+        result=exp1_model.run()
+        recorder.insert(result)
+    
+    
+    write_result(description,exp1_model.settings,recorder.statistic)
 
 
 
